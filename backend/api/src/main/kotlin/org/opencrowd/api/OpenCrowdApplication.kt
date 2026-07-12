f561a08c595a9442108dbf097b7f256708f396bc
mike <b2bmichael@gmail.com>
2026-07-12 23:15:01 +0300

    fix(backend): add EntityScan and EnableJpaRepositories for cross-module scanning

--- backend/api/src/main/kotlin/org/opencrowd/api/OpenCrowdApplication.kt
+++ backend/api/src/main/kotlin/org/opencrowd/api/OpenCrowdApplication.kt
@@ -1,9 +1,13 @@
 package org.opencrowd.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["org.opencrowd"])
+@EntityScan(basePackages = ["org.opencrowd.core.entity"])
+@EnableJpaRepositories(basePackages = ["org.opencrowd.core.repository"])
 class OpenCrowdApplication
 
 fun main(args: Array<String>) {
